a8a91f0f33a8c6889d08fdef321970e8ba59ed17
mutantsan <kvaqich@gmail.com>
2022-02-02 14:47:34 +0200

    move actions to logic/action

---
+++ ckanext/transmute/logic/auth/__init__.py
@@ -0,0 +1,2 @@
+def get_auth_functions():
+    return {}
--- ckanext/transmute/plugin.py
+++ ckanext/transmute/plugin.py
@@ -1,7 +1,7 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
-from ckanext.transmute.actions import get_actions
+from ckanext.transmute.logic.action import get_actions
 from ckanext.transmute.cli import get_commands
 
 
@@ -11,7 +11,6 @@ class TransmutePlugin(p.SingletonPlugin):
     p.implements(p.IClick)
 
     # IConfigurer
-
     def update_config(self, config_):
         tk.add_template_directory(config_, 'templates')
         tk.add_public_directory(config_, 'public')
@@ -19,15 +18,12 @@ class TransmutePlugin(p.SingletonPlugin):
             'transmute')
 
     # IActions
-
     def get_actions(self):
         """Registers a list of extension specific actions
         """
         return get_actions()
     
-    
     # IClick
-
     def get_commands(self):
         """Registers a list of extension specific CLI commands
         """
