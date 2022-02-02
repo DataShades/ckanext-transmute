aa5181ee727998626084ba80d08ff3bfa53d5535
mutantsan <kvaqich@gmail.com>
2022-02-02 13:31:49 +0200

    use IClick interface

--- ckanext/transmute/plugin.py
+++ ckanext/transmute/plugin.py
@@ -2,11 +2,13 @@ import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
 from ckanext.transmute.actions import get_actions
+from ckanext.transmute.cli import get_commands
 
 
 class TransmutePlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
     p.implements(p.IActions)
+    p.implements(p.IClick)
 
     # IConfigurer
 
@@ -19,4 +21,14 @@ class TransmutePlugin(p.SingletonPlugin):
     # IActions
 
     def get_actions(self):
+        """Registers a list of extension specific actions
+        """
         return get_actions()
+    
+    
+    # IClick
+
+    def get_commands(self):
+        """Registers a list of extension specific CLI commands
+        """
+        return get_commands()
